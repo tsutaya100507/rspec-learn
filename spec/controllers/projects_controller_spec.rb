d2c356f61052264b188742380a5013af2c4aa563
Tatsuya Ichikawa <>
2020-01-19 23:28:30 +0900

    test controller update

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -72,10 +72,8 @@ RSpec.describe ProjectsController, type: :controller do
         project_params = FactoryBot.attributes_for(:project)
         sign_in @user
         expect {
-          post :create, params: {
-            project: project_params
+          post :create, params: { project: project_params }
           }.to change(@user.projects, :count).by(1)
-        }
       end
     end
 
@@ -87,7 +85,7 @@ RSpec.describe ProjectsController, type: :controller do
       end
 
       it "redirects to the sign-in page" do
-        projects_params = FactoryBot.attributes_for(:project)
+        project_params = FactoryBot.attributes_for(:project)
         post :create, params: { project: project_params }
         expect(response).to redirect_to "/users/sign_in"
       end
@@ -114,6 +112,39 @@ RSpec.describe ProjectsController, type: :controller do
         @user = FactoryBot.create(:user)
         other_user = FactoryBot.create(:user)
         @project = FactoryBot.create(:project, owner: other_user, name: "Same Old Name")
+      end
+
+      it "does not update the project" do
+        project_params = FactoryBot.attributes_for(:project, name: "New Name")
+        sign_in @user
+        patch :update, params: { id: @project.id, project: project_params }
+        expect(@project.reload.name).to eq "Same Old Name"
+      end
+
+      it "redirects to the dashboard" do
+        project_params = FactoryBot.attributes_for(:project)
+        sign_in @user
+        patch :update, params: { id: @project.id, projet: project_params}
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "as a guest" do
+      before do
+        @project = FactoryBot.create(:project)
+      end
+
+      it "returns a 302 response" do
+        project_params = FactoryBot.attributes_for(:project)
+        patch :update, params: { id: @project.id, project: project_params }
+        # 302はリダイレクト
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirects to the sign-in page" do
+        project_params = FactoryBot.attributes_for(:project)
+        patch :update, params: { id: @project.id, project: project_params }
+        expect(response).to redirect_to "/users/sign_in"
       end
     end
   end
